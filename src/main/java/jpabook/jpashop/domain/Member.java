fafa73edb6aba26dcd8b4503678dad64eb81df65
mine <mineki94@naver.com>
2024-03-15 00:20:07 +0900

    엔티티 기본설정

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,60 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue // (strategy = GenerationType.AUTO) 생략하면 기본값이 AUTO
+    @Column(name = "MEMBER_ID") // 대소문자는 회사마다 룰이 조금씩 다름,
+    private Long id;
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
