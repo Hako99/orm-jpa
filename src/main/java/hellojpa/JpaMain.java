7a8da1d6409b786c37e0516c55fcc9b54cc8e100
mine <mineki94@naver.com>
2024-03-14 20:11:59 +0900

    영속성 컨텍스트 따라해보기

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,41 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member1 = new Member();
+            member1.setName("A");
+            Member member2 = new Member();
+            member2.setName("B");
+            Member member3 = new Member();
+            member3.setName("C");
+
+            System.out.println("======================");
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+            System.out.println(member1.getId());
+            System.out.println(member2.getId());
+            System.out.println(member3.getId());
+
+            System.out.println("======================");
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
